99df79ff74a1a46c0251b1b22605a8e1f721c151
Fengguang <14812038+fengguangk@user.noreply.gitee.com>
2024-08-30 17:03:13 +0800

    更改spider函数中的返回类型

--- spider/item.py
+++ spider/item.py
@@ -1,21 +1,17 @@
 '''暂时用类存放数据，因为我不知道最后算法要什么，这样方便修改'''
-class Comment:
-    def __init__(self,score = None,user_name = None,user_id = None,user_href = None,comment_content = None,vote_count=None) :
+class Comment:#短评评论
+    def __init__(self,score = None,user_name = None,user_href = None,comment_content = None,vote_count=None) :
         self.score=score
-        self.user_name = str(user_name)
-        self.user_id = str(user_id)
-        self.user_href = str(user_href)
-        self.vote_count = str(vote_count)
-        self.comment_content = str(comment_content)
+        self.user_name = user_name#用户名称
+        self.user_href = user_href#用户个人主页
+        self.vote_count = vote_count#短评评价有用数
+        self.comment_content = comment_content#短评内容
         
 class Review:
-    def __init__(self,title = None,score = None,user_name = None,user_id = None,user_href = None,\
+    def __init__(self,score = None,user_name = None,user_href = None,\
         comment_content = None,useful_count = None,useless_count = None) :
-        self.title=title
         self.score=score
         self.user_name = user_name
-        self.user_id = user_id
         self.user_href = user_href
-        self.useful_count = useful_count
-        self.useless_count = useless_count
-        self.comment_content = comment_content
+        self.useful_count = useful_count #乐评的有用数
+        self.useless_count = useless_count#乐评的无用数
